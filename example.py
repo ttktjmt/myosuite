95b1f20582ac02a7c7b9818c347661943c23eaf1
ttktjmt <ta747375ki@gmail.com>
2024-09-26 17:56:22 +0900

    Create a new env 'myoChallengeBimanualOneArm-v0' without MPL

--- example.py
+++ example.py
@@ -3,9 +3,9 @@ from myosuite.utils import gym
 
 print(f"version: {myosuite.__version__}")
 
-env = gym.make('myoChallengeBimanual-v0')
+env = gym.make('myoChallengeBimanualOneArm-v0')
 env.reset()
 for _ in range(10000):
-  # env.mj_render()
+  env.mj_render()
   env.step(env.action_space.sample()) # take a random action
 env.close()
