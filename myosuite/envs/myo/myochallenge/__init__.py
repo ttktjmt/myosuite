c0bff1b4050f8d49fec8238335b2dcc0e08bc69d
ttktjmt <ta747375ki@gmail.com>
2024-09-26 17:56:22 +0900

    Create a new env 'myoChallengeBimanualOneArm-v0' without MPL

--- myosuite/envs/myo/myochallenge/__init__.py
+++ myosuite/envs/myo/myochallenge/__init__.py
@@ -30,12 +30,24 @@ def register_env_with_variants(id, entry_point, max_episode_steps, kwargs):
             silent=True
         )
 
-
+# MyoChallenge 2024 envs ==============================================
+# MyoChallenge Manipulation P2
 register_env_with_variants(id='myoChallengeBimanual-v0',
         entry_point='myosuite.envs.myo.myochallenge.bimanual_v0:BimanualEnvV1',
         max_episode_steps=250,
         kwargs={
             'model_path': curr_dir + '/../assets/arm/myoarm_bionic_bimanual.xml',
+            'normalize_act': True,
+            'frame_skip': 5,
+        }
+    )
+
+# !!! Custom MyoChallenge Manipulation P2
+register_env_with_variants(id='myoChallengeBimanualOneArm-v0',
+        entry_point='myosuite.envs.myo.myochallenge.bimanual_onearm_v0:BimanualEnvV1',
+        max_episode_steps=250,
+        kwargs={
+            'model_path': curr_dir + '/../assets/arm/myoarm_bionic_bimanual_onearm.xml',
             'normalize_act': True,
             'frame_skip': 5,
         }
