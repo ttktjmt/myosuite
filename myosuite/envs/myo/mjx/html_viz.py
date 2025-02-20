9118510b651b37edf5413954c62bf8d23d4b7dde
ttktjmt <ta747375ki@gmail.com>
2025-02-20 13:24:13 +0900

    Clean up the html_viz.py
    update index.html

--- myosuite/envs/myo/mjx/html_viz.py
+++ myosuite/envs/myo/mjx/html_viz.py
@@ -1,24 +1,13 @@
-from datetime import datetime
-import functools
 import jax
-from jax import numpy as jp
-from matplotlib import pyplot as plt
-import mujoco
-from mujoco import mjx
+from jax import numpy as jnp
 from brax import envs
-from brax.envs.base import Env, PipelineEnv, State
 from brax.training.acme.running_statistics import normalize
 from brax.training.agents.ppo import networks as ppo_networks
-from brax.training.agents.ppo import train as ppo
-from brax.io import mjcf, model, html
-import mujoco.viewer
-
+from brax.io import model, html
+from tqdm import tqdm
 from elbow import Elbow
 
-print(f"Current backend: {jax.default_backend()}")
-
-xml = '../assets/elbow/myoelbow_1dof6muscles_mjx_eval.xml'
-
+# Create PPO network and load parameters
 ppo_network = ppo_networks.make_ppo_networks(
       4,
       6,
@@ -26,59 +15,46 @@ ppo_network = ppo_networks.make_ppo_networks(
 model_path = 'elbow_params.pickle'
 params = model.load_params(model_path)
 
-def deterministic_policy(input_data):
-    logits = ppo_network.policy_network.apply(*params[:2], jp.array([input_data], dtype=jp.float32))
+def deterministic_policy(input_data: jnp.ndarray) -> jnp.ndarray:
+    logits = ppo_network.policy_network.apply(*params[:2], jnp.array([input_data], dtype=jnp.float32))
     brax_result = ppo_network.parametric_action_distribution.mode(logits)
     return brax_result[0] # remove the batch dimension
 
-def get_obs(data, target):
+def get_obs(data, target: jnp.ndarray) -> jnp.ndarray:
     """Observes elbow angle, velocities, and last applied torque."""
     position = data.qpos
 
     # external_contact_forces are excluded
-    return jp.concatenate([
+    return jnp.concatenate([
         position,
         data.qvel,
         data.qfrc_actuator,
         target
     ])
 
-def arm_control(model, data):
-    """
-    :type model: mujoco.MjModel
-    :type data: mujoco.MjData
-    """
-    # `model` contains static information about the modeled system, e.g. their indices in dynamics matrices
-    # `data` contains the current dynamic state of the system
-    observations = get_obs(data, [data.ctrl[-1]])
-    data.ctrl[:-1] = deterministic_policy(observations)
-    pass
-
-def main(is_msk=True):
+def main(is_msk: bool = True) -> None:
     envs.register_environment('elbow', Elbow)
     env = envs.get_environment('elbow', is_msk=is_msk)
     jit_reset = jax.jit(env.reset)
     jit_step = jax.jit(env.step)
 
-    # initialize the state
-    rng = jax.random.PRNGKey(0)
+    rng = jax.random.PRNGKey(5)
     state = jit_reset(rng)
     rollout = [state.pipeline_state]
 
     n_steps = 500
-    for step in range(n_steps):
+    for step in tqdm(range(n_steps), desc='Simulation'):
         if state.done or step % 100 == 0:
             state = jit_reset(state.info['rng'])
-        observations = get_obs(state.pipeline_state, state.info['target_angle'])
-        action = deterministic_policy(observations)
+        obs = get_obs(state.pipeline_state, state.info['target_angle'])
+        action = deterministic_policy(obs)
         state = jit_step(state, action)
         rollout.append(state.pipeline_state)
     
     # Save the trajectory as an HTML file
-    render_every = 2
     html_content = html.render(
         env.sys.tree_replace({'opt.timestep': env.dt}),
-        rollout[::render_every],
+        rollout,
         height=850,
     )
     with open('elbow.html', 'w') as f:
